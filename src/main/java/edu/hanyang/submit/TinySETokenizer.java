5bfb3d7e21f6f79349c2f19c4e86532082e6dd98
Frank Ridder <fr.ridder@hotmail.com>
2019-03-25 16:40:17 +0900

    test

--- src/main/java/edu/hanyang/submit/TinySETokenizer.java
+++ src/main/java/edu/hanyang/submit/TinySETokenizer.java
@@ -1,19 +1,31 @@
 package edu.hanyang.submit;
 
-import java.util.List;
+import java.util.*;
 
 import edu.hanyang.indexer.Tokenizer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.tartarus.snowball.ext.PorterStemmer;
+
 
 public class TinySETokenizer implements Tokenizer {
-
+	private SimpleAnalyzer analyzer;
+	private PorterStemmer stemmer;
 	public void setup() {
+		analyzer = new SimpleAnalyzer();
+		stemmer = new PorterStemmer();
 	}
 
 	public List<String> split(String text) {
+		List<String> finishedList = new ArrayList<>();
+		String tokenizedText = "";
+		analyzer.tokenStream(text,tokenizedText);
+		System.out.println(tokenizedText);
+
 		return null;
 	}
 
 	public void clean() {
+		analyzer.close();
 	}
 
 }
